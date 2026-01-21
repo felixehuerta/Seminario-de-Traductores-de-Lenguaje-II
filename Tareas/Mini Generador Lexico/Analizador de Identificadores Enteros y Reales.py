8ba683fe07a7be3daea5803a380e8551d55e5cd4
felixehuerta <felix.ehuerta@alumnos.udg.mx>
2026-01-21 15:41:05 -0600

    Realización de un analizador lexico sencillo que detecte ID y Reales

--- Tareas/Mini Generador Lexico/Analizador de Identificadores Enteros y Reales.py
+++ Tareas/Mini Generador Lexico/Analizador de Identificadores Enteros y Reales.py
@@ -1,4 +1,29 @@
 """
 Analizador sintactico de identificadores enteros y reales
-"""
 
+Instrucciones:
+1.- Genera un pequeño analizador léxico, que identifique los 
+siguientes tokens 
+    * identificadores = letra(letra|digito)*
+    * Real = entero.entero+
+"""
+import re
+
+# Definición de Reglas
+TOKEN_ID = r'^[a-zA-Z][a-zA-Z0-9]*$'
+TOKEN_REAL = r'^[0-9]+\.[0-9]+$'
+
+def analizar_lexico(entrada):
+    tokens = entrada.split()
+    
+    for palabra in tokens:
+        if re.match(TOKEN_REAL, palabra):
+            print(f"{palabra} -> REAL")
+        elif re.match(TOKEN_ID, palabra):
+            print(f"{palabra} -> IDENTIFICADOR")
+        else:
+            print(f"{palabra} -> ERROR LÉXICO")
+
+# Ejecución del Programa
+entrada = input("Cadena a analizar: ")
+analizar_lexico(entrada)
